ccd9e2e282286d7e10f57ee8f643db04957fbf44
sparkam04 <sparkam@rambler.ru>
2017-06-16 01:03:34 +0300

    Update hotel (in progress.)

--- src/main/resources/static/js/testCtrl.js
+++ src/main/resources/static/js/testCtrl.js
@@ -10,7 +10,6 @@
         this.hotel = {};
         this.editedHotel = {};
 
-
         var self = this;
         HotelDataSvc.getHotels()
         .then(function(data) {
@@ -34,6 +33,7 @@
 
         this.selectHotel = function (index) {
             this.hotel = this.hotels[index];
+            this.hotel.fullAddess = this.getFullAddressByLocationId(this.hotel.locationId);
         };
 
         this.selectCountry = function () {
@@ -47,11 +47,43 @@
         this.selectLocation = function () {
         };
 
+        this.getFullAddressByLocationId = function (locationId) {
+            var _location = ($filter('filter')(this.locations, {'id': locationId}))[0];
+            var _city = ($filter('filter')(this.cities, {'id': _location.cityId}))[0];
+            var _country = ($filter('filter')(this.countries, {'id': _city.countryId}))[0];
+
+            return {
+                'country': _country,
+                'city': _city,
+                'location': _location
+            };
+
+        };
+
         this.updateHotel = function (hotelData) {
+            if(hotelData.cInTime === undefined) {
+                var t1 = hotelData.checkInTime.split(':');
+                var d1 = new Date();
+                d1.setHours(t1[0]);
+                d1.setMinutes(t1[1]);
+                d1.setSeconds(0);
+                d1.setMilliseconds(0);
+                hotelData.cInTime = new Date(d1);
+            }
+            if(hotelData.cOutTime === undefined) {
+                var t2 = hotelData.checkOutTime.split(':');
+                var d2 = new Date();
+                d2.setHours(t2[0]);
+                d2.setMinutes(t2[1]);
+                d2.setSeconds(0);
+                d2.setMilliseconds(0);
+                hotelData.cOutTime = new Date(d2);
+            }
+
             hotelData.checkInTime = $filter('date')(hotelData.cInTime, 'HH:mm:ss').toString();
             hotelData.checkOutTime = $filter('date')(hotelData.cOutTime, 'HH:mm:ss').toString();
 
-            console.log(JSON.stringify(hotelData.valueOf()));
+            // console.log(JSON.stringify(hotelData.valueOf()));
             HotelDataSvc.updHotel(hotelData);
         };
 
